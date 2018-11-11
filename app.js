0c562878aa5814499ce1d74bf3ec95471dcae640
uranoni <uranoni777@gmail.com>
2018-11-11 14:35:38 +0800

    create model bascic api foe signup login with jwt

--- app.js
+++ app.js
@@ -1,29 +1,52 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const {User} = require('./models/user.js');
 const moment = require('moment-timezone');
-
+const {ObjectID} = require('mongodb')
+const {authenticate} = require('./middleware/authenticate.js')
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TutRatingSystem', { useMongoClient: true });
 
 
 var app = express();
 app.use(bodyParser.json());
 
-app.get('/',(req,res,next)=>{
-  res.status(200).json({
-    message:"hello world"
-  });
-});
 
-app.post('/',(req,res,next)=>{
-  const person={
-    name:req.body.name,
-    age:req.body.age
-  }
-  
-  res.status(200).json({
-    message:"person data",
-    createPerson:person
-  });
+//註冊
+app.post('/signup',(req, res) => {
+  var body = _.pick(req.body, ['email', 'password', 'name', 'phone', 'studentId', 'department', 'lineId', 'roleId'])
+  body.time = new Date().toString();
+  var user = new User(body);
+  user.save().then(() => {
+    user.generateAuthToken();
+  }).then((token) => {
+    res.header('authToken', token).send(user.name);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+//登入
+app.post('/signin', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('authToken', token).send();
+    }).catch(() => {
+      res.status(403).send();
+    })
+  })
 })
+
+app.get('/me', authenticate, (req, res) => {
+  var user = req.user
+  var objUser = user.toJson()
+  res.send(objUser);
+})
+
+
+
 app.listen(3000, () => {
   var moment = require('moment-timezone');
 console.log(moment().tz("Asia/Taipei").format())
---
+++ middleware/authenticate.js
@@ -0,0 +1,19 @@
+
+var {User} = require('./../models/user.js')
+
+var authenticate = (req, res, next) => {
+  var token = req.header('authToken');
+  User.findByToken(token).then(user => {
+    if (!user) {
+      return Promise.reject();
+    }
+      req.user = user;
+      req.token = token;
+      //console.log(req.user.roleId);
+      next();
+  }).catch(()=>{
+    res.status(401).send();
+  })
+}
+
+module.exports = {authenticate};
